d13fa213908dfbb204e4a24448982395f54b7cef
kiranCC21 <125582803+kiranCC21@users.noreply.github.com>
2026-09-22 14:52:19 +0530

    edit
    edit

--- src/data/expData.jsx
+++ src/data/expData.jsx
@@ -1,21 +1,21 @@
-export const EXP_DATA = [
-   {
-    category: "Yaticorp India Private Limited",
-    Company_Name: [
-      "Company Name: " + Company_Name[0], 
-      "Duration: 16 December 2024 - 31 December 2025",
-      "Designation: Full Stack Developer & Automation Team Lead",
-      "Description: Led full-stack web development and automation initiatives in a fast-paced startup environment. Spearheaded the migration of core web platforms from WordPress to React, integrated secure APIs with MongoDB, and built custom dashboards. Managed end-to-end EdTech operations, including LMS administration. Additionally, directed the WhatsApp Automation team using Gallabox to scale learner onboarding, event communication, and cross-functional business workflows."
-    ],
-  },
-    {
-    category: "Concentrix Services India Private Limited",
-    Company_Name: [
-      "Company Name: " + Company_Name[1],  
-      "Duration: 6 July 2026 - Present",
-    "Designation: Technical support Advisor",
-    "Description: Deliver high-tier technical support and troubleshooting solutions for complex software, hardware, and network issues. Act as a critical bridge between users and technical infrastructure by diagnosing system anomalies, guiding cross-functional resolutions, and ensuring maximum uptime. Utilize deep analytical problem-solving skills to optimize system performance and maintain industry-leading service delivery standards."
-    ,
-    ],
-    }
-];
+// export const EXP_DATA = [
+//    {
+//     category: "Yaticorp India Private Limited",
+//     Company_Name: [
+//       "Company Name: " + Company_Name[0], 
+//       "Duration: 16 December 2024 - 31 December 2025",
+//       "Designation: Full Stack Developer & Automation Team Lead",
+//       "Description: Led full-stack web development and automation initiatives in a fast-paced startup environment. Spearheaded the migration of core web platforms from WordPress to React, integrated secure APIs with MongoDB, and built custom dashboards. Managed end-to-end EdTech operations, including LMS administration. Additionally, directed the WhatsApp Automation team using Gallabox to scale learner onboarding, event communication, and cross-functional business workflows."
+//     ],
+//   },
+//     {
+//     category: "Concentrix Services India Private Limited",
+//     Company_Name: [
+//       "Company Name: " + Company_Name[1],  
+//       "Duration: 6 July 2026 - Present",
+//     "Designation: Technical support Advisor",
+//     "Description: Deliver high-tier technical support and troubleshooting solutions for complex software, hardware, and network issues. Act as a critical bridge between users and technical infrastructure by diagnosing system anomalies, guiding cross-functional resolutions, and ensuring maximum uptime. Utilize deep analytical problem-solving skills to optimize system performance and maintain industry-leading service delivery standards."
+//     ,
+//     ],
+//     }
+// ];
