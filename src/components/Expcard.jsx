b320def24a6739c9585e08dd399749b12b8958da
kiranCC21 <125582803+kiranCC21@users.noreply.github.com>
2026-09-22 13:45:43 +0530

    exp
    exp update

---
+++ src/components/Expcard.jsx
@@ -0,0 +1,48 @@
+
+
+import { motion } from "framer-motion";
+
+const ExcpCard = ({ exp }) => {
+
+  return (
+    <motion.div
+      initial={{ y: 30, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.4 }}
+      className="w-[85%] md:w-[70%] bg-white p-8 rounded-2xl shadow-lg border"
+    >
+
+      <div className="w-12 h-1 bg-green-600 mb-4 rounded-full" />
+
+      <h2 className="text-2xl font-semibold mb-3">
+        {exp.title}
+      </h2>
+
+      <p className="text-gray-600 mb-4">
+        {exp.desc}
+      </p>
+
+      <div className="grid md:grid-cols-3 gap-4 text-sm">
+
+        <div>
+          <p className="font-medium">Role</p>
+          {exp.role}
+        </div>
+
+        <div>
+          <p className="font-medium">Tech</p>
+          {exp.tech}
+        </div>
+
+        <div>
+          <p className="font-medium">Impact</p>
+          {exp.impact}
+        </div>
+
+      </div>
+
+    </motion.div>
+  );
+};
+
+export default ExpCard;
